d7821078dbf2e384b63ec329c2b6e8d1c5517de5
toghrul-guluzade <tguluzade19619@ada.edu.az>
2026-03-23 18:08:44 +0400

    feat: WRITE_BIT added to bit_utils.h

--- include/common/bit_utils.h
+++ include/common/bit_utils.h
@@ -3,7 +3,7 @@
  * @file bit_utils.h
  * @brief Basic bit manipulation macros for register-level operations.
  *
- * Provides common utilities for setting, clearing, toggling, and reading
+ * Provides common utilities for setting, clearing, toggling, reading, and writing
  * individual bits in hardware registers.
  */
 
@@ -51,6 +51,19 @@
  * @return Non-zero if bit is set, 0 if bit is clear.
  */
 #define READ_BIT(reg, bit_position) ((reg) & (1 << (bit_position)))
+
+
+/**
+ * @brief Write a specific bit in a register.
+ *
+ * Sets or clears the specified bit depending on the provided value.
+ * If the value is non-zero, the bit is set; otherwise, the bit is cleared.
+ *
+ * @param reg Target register.
+ * @param bit_position Bit index to modify.
+ * @param value Value to write (non-zero = set, 0 = clear).
+ */
+#define WRITE_BIT(REG, BIT, VAL)    ((VAL) ? SET_BIT((REG), (BIT)) : CLEAR_BIT((REG), (BIT)))
 
 
 #endif // BIT_UTILS_H
